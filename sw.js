d4c774cedf77fd6002ba883146a3e579ed439df8
Umberto96 <95257464+Umberto96@users.noreply.github.com>
2026-02-28 15:44:08 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-const CACHE = 'dispensa-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = "dispensa-v1";
+const ASSETS = ["/", "/index.html", "/manifest.json"];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
+self.addEventListener("activate", e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match("/index.html")))
   );
 });
